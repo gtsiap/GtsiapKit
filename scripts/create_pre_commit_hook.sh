0401ce170c20695c781b2fe8ce0504dcd0e6128a
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-07-18 19:41:01 +0300

    fix the spaces script

---
+++ scripts/create_pre_commit_hook.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ ! -d .git/hooks ]; then
+    mkdir .git/hooks
+fi
+
+if [ ! -L .git/hooks/pre-commit ]; then
+    ln -s $PWD/scripts/remove_spaces.sh $PWD/.git/hooks/pre-commit
+
+    chmod +x .git/hooks/pre-commit
+fi
--- scripts/remove_spaces.sh
+++ scripts/remove_spaces.sh
@@ -1,21 +1,14 @@
 #!/usr/bin/env sh
 
-if [[ -z $1 ]]; then
-    echo "SRCROOT is missing"
-    exit 1
-fi
-
-if [[ ! -d $1 ]]; then
-    echo "$1 doesn't exist"
-    exit 1
-fi
+SRCROOT=$(git rev-parse --show-toplevel)
 
 echo "Removing spaces from files"
 
 # http://stackoverflow.com/questions/7039130/bash-iterate-over-list-of-files-with-spaces
 
-find . -iname "*.swift" | while read f
+find $SRCROOT -iname "*.swift" | while read f
 do
-    echo $f
     sed -i "" -e 's/[[:blank:]]*$//g' "$f"
 done
+
+echo "done"
