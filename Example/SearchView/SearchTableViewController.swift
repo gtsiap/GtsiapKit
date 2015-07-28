a6e2fa92ed444c610ee49b6bf042dcc8f72aa84a
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-07-28 13:16:25 +0300

    initial commit for SearchView
    SearchView is a UISearchBar with a custom subview

---
+++ Example/SearchView/SearchTableViewController.swift
@@ -0,0 +1,104 @@
+//
+//  SearchTableViewController.swift
+//  GtsiapKit
+//
+//  Created by Giorgos Tsiapaliokas on 7/28/15.
+//  Copyright (c) 2015 Giorgos Tsiapaliokas. All rights reserved.
+//
+
+import UIKit
+import GtsiapKit
+
+class SearchTableViewController: UITableViewController {
+
+    private let data = ["one", "two", "three"]
+    private lazy var searchView: SearchView = {
+        let searchView = SearchView()
+        searchView.viewController = self
+        searchView.contentView = self.contentView
+        searchView.didStartSearching = {
+            self.sizeHeaderToFit()
+        }
+
+        searchView.didStopSearching = {
+            self.sizeHeaderToFit()
+        }
+
+        return searchView
+    }()
+
+    private lazy var contentView: UIView = {
+        let contentView = UIView()
+
+        let label1 = ThemeManager.defaultTheme.label()
+        label1.setTranslatesAutoresizingMaskIntoConstraints(false)
+        label1.text = "Label1"
+        label1.preferredMaxLayoutWidth = self.tableView.frame.width
+
+        let label2 = ThemeManager.defaultTheme.label()
+        label2.setTranslatesAutoresizingMaskIntoConstraints(false)
+        label2.text = "Label2"
+        label2.preferredMaxLayoutWidth = self.tableView.frame.width
+
+        contentView.addSubview(label1)
+        contentView.addSubview(label2)
+
+        let l1TopConstraint = contentView.constraint(label1, attribute1: .Top)
+        let l1CenterXConstraint = contentView.constraint(label1, attribute1: .CenterX)
+
+        let l2TopConstraint = contentView.constraint(label2, attribute1: .Top, view2: label1, attribute2: .Bottom, multiplier: 1.1)
+        let l2CenterXConstraint = contentView.constraint(label2, attribute1: .CenterX)
+        let l2BottomConstraint = contentView.constraint(label2, attribute1: .Bottom)
+
+        contentView.addConstraints([
+            l1TopConstraint,
+            l1CenterXConstraint,
+            l2TopConstraint,
+            l2CenterXConstraint,
+            l2BottomConstraint
+        ])
+
+        return contentView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.tableHeaderView = self.searchView
+        sizeHeaderToFit()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as! UITableViewCell
+
+        cell.textLabel?.text = self.data[indexPath.row]
+
+        return cell
+    }
+
+    func sizeHeaderToFit() {
+        let headerView = tableView.tableHeaderView!
+
+        headerView.setNeedsLayout()
+        headerView.layoutIfNeeded()
+
+        let height = headerView.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize).height
+        println("height: \(height)")
+        var frame = headerView.frame
+        frame.size.height = height
+        headerView.frame = frame
+
+        tableView.tableHeaderView = headerView
+    }
+}
---
+++ GtsiapKit/SearchView/SearchView.swift
@@ -0,0 +1,135 @@
+//
+//  SearchView.swift
+//  GtsiapKit
+//
+//  Created by Giorgos Tsiapaliokas on 7/28/15.
+//  Copyright (c) 2015 Giorgos Tsiapaliokas. All rights reserved.
+//
+
+import UIKit
+
+public class SearchView: UIView {
+
+    // MARK: vars
+    public var contentView: UIView? {
+        didSet {
+            if self.contentView == nil {
+                return
+            }
+
+            oldValue?.removeFromSuperview()
+
+            self.contentView!.setTranslatesAutoresizingMaskIntoConstraints(false)
+        }
+    }
+
+    public let searchBar: UISearchBar = UISearchBar()
+    public var viewController: UIViewController?
+
+    // MARK: closures
+
+    public var didStartSearching: (() -> ())?
+    public var didStopSearching: (() -> ())?
+
+    // MARK: private vars
+    private var searchBarBottomConstraint: NSLayoutConstraint!
+    private var contentViewBarBottomConstraint: NSLayoutConstraint!
+
+    // MARK: init
+
+    public init() {
+        super.init(frame: CGRectZero)
+
+        initSearchBar()
+
+        addSubview(self.searchBar)
+        self.searchBar.setTranslatesAutoresizingMaskIntoConstraints(false)
+
+        let widthConstraint = constraint(self.searchBar, attribute1: .Width)
+        let centerXConstraint = constraint(self.searchBar, attribute1: .CenterX)
+        let topConstraint = constraint(self.searchBar, attribute1: .Top)
+
+        self.searchBarBottomConstraint = constraint(self.searchBar, attribute1: .Bottom)
+
+        addConstraints([widthConstraint, centerXConstraint, topConstraint, searchBarBottomConstraint])
+    }
+
+    required public init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func updateConstraints() {
+
+        if self.contentViewBarBottomConstraint != nil {
+            NSLayoutConstraint.deactivateConstraints([self.searchBarBottomConstraint])
+            addConstraint(self.contentViewBarBottomConstraint)
+        } else {
+            NSLayoutConstraint.activateConstraints([self.searchBarBottomConstraint])
+        }
+
+        super.updateConstraints()
+    }
+
+    // MARK: private funcs
+
+    private func initSearchBar() {
+        self.searchBar.delegate = self
+    }
+
+    private func showContentView() {
+        addSubview(self.contentView!)
+        self.contentView!.hidden = false
+
+        let widthConstraint = constraint(self.contentView!, attribute1: .Width)
+        let centerXConstraint = constraint(self.contentView!, attribute1: .CenterX)
+
+        let topConstraint = constraint(self.contentView!, attribute1: .Top, view2: self.searchBar, attribute2: .Bottom, multiplier: 1.1)
+        self.contentViewBarBottomConstraint = constraint(self.contentView!, attribute1: .Bottom)
+
+        addConstraints([widthConstraint, centerXConstraint, topConstraint])
+
+        setNeedsUpdateConstraints()
+
+        self.didStartSearching?()
+    }
+
+    private func hideContentView() {
+        self.contentViewBarBottomConstraint = nil
+
+        self.contentView?.removeFromSuperview()
+        setNeedsUpdateConstraints()
+    }
+
+    private func showControls() {
+        self.viewController?.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.searchBar.setShowsCancelButton(true, animated: false)
+    }
+
+    private func hideControls() {
+        self.viewController?.navigationController?.setNavigationBarHidden(false, animated: true)
+        self.searchBar.setShowsCancelButton(false, animated: true)
+    }
+}
+
+// MARK: -
+// MARK: UISearchBarDelegate
+
+extension SearchView: UISearchBarDelegate {
+
+    public func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.showContentView()
+        self.showControls()
+    }
+
+    public func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        hideControls()
+        hideContentView()
+
+        self.didStopSearching?()
+    }
+
+    public func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        self.searchBar.text = ""
+        self.searchBar.endEditing(true)
+    }
+}
