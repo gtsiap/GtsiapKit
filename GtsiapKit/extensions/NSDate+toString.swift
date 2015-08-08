66d9a339cabf801368da1693523878cb2e5ea697
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-08-08 18:21:41 +0300

    add NSDate+toString

---
+++ GtsiapKit/extensions/NSDate+toString.swift
@@ -0,0 +1,19 @@
+//
+//  NSDate+toString.swift
+//  GtsiapKit
+//
+//  Created by Giorgos Tsiapaliokas on 8/8/15.
+//  Copyright (c) 2015 Giorgos Tsiapaliokas. All rights reserved.
+//
+
+extension NSDate {
+
+    public var toString: String {
+        let formatter = NSDateFormatter()
+        formatter.locale = NSLocale.currentLocale()
+        formatter.dateFormat = "HH:mm dd/MM/yy"
+        return formatter.stringFromDate(self)
+    }
+
+}
+
