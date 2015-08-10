2b322ed4bc5d1cd93279ee2a2ed45ec27fc76e28
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-08-10 16:01:27 +0300

    add Example-iOS

---
+++ Example-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example-iOS
+//
+//  Created by Giorgos Tsiapaliokas on 8/10/15.
+//  Copyright (c) 2015 Giorgos Tsiapaliokas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
