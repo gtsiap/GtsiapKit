da2848ccbfbcbf0d74fc364fc4ccd89efd9d5c9a
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-08-26 16:50:06 +0300

    add OperationQueue

---
+++ GtsiapKit/Operation/OperationQueue.swift
@@ -0,0 +1,33 @@
+//
+//  OperationQueue.swift
+//  GtsiapKit
+//
+//  Created by Giorgos Tsiapaliokas on 8/26/15.
+//  Copyright (c) 2015 Giorgos Tsiapaliokas. All rights reserved.
+//
+
+public class OperationQueue: NSOperationQueue {
+
+    public var operationsDidFinish: (() -> ())?
+
+    public override init() {
+        super.init()
+
+        addObserver(self, forKeyPath: "operationCount", options: NSKeyValueObservingOptions.New, context: nil)
+    }
+
+    public override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+
+        if keyPath != "operationCount" {
+            return
+        }
+
+        if let queue = object as? NSOperationQueue {
+            if queue.operationCount == 0 {
+                self.operationsDidFinish?()
+            }
+        }
+
+    }
+
+}
