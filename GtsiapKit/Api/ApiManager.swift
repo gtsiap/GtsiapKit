1fac2d6f2886192b20f72cb5a1d1780556e84584
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-09-23 19:30:56 +0300

    adopt protocol oriented programming for ApiTask and friends

--- GtsiapKit/Api/ApiManager.swift
+++ GtsiapKit/Api/ApiManager.swift
@@ -5,9 +5,9 @@
 //  Copyright (c) 2015 Giorgos Tsiapaliokas. All rights reserved.
 //
 
-import Alamofire
-
 let networkLogger = Logger.defaultLogger(["network"])
+
+import Alamofire
 
 public struct UserCredentials {
     public let email: String
@@ -32,83 +32,11 @@ public class ApiManager {
         Manager.sharedInstance.startRequestsImmediately = false
     }
 
-    func doRequest(urlRequest: URLRequestConvertible,
-        completionHandler: (data: AnyObject?) -> Void) -> ApiTask {
-
-        let task = ApiTask()
-        task.request = request(urlRequest).response {
-            (request, response, data, error) in
-
-             // in future versions check for errors
-             networkLogger.debug("request: \(request)")
-             networkLogger.debug("response: \(response)")
-             networkLogger.debug("error: \(error)")
-
-            var jsonError: NSError?
-            let jsonData: AnyObject?
-            do {
-                jsonData = try NSJSONSerialization.JSONObjectWithData(data!,
-                                options: NSJSONReadingOptions.AllowFragments)
-            } catch let error as NSError {
-                jsonError = error
-                jsonData = nil
-            } catch {
-                fatalError()
-            }
-
-            networkLogger.debug("data: \(jsonData)")
-
-            var networkError: NSError?
-
-            // TODO
-            if let error = error as? NSError {
-
-                if error.code == -1009 {
-                    // no internet connection is available
-                    self.isOffline = true
-
-                    if let storedData: AnyObject = self.retrieveRequest(request!) {
-                        completionHandler(data: storedData)
-                        task.requestDidFinish()
-
-                        return
-                    }
-
-                    task.requestDidFinishWithNoNetworkConnection()
-                    return
-                }
-
-                networkError = error
-
-            } else if jsonError != nil {
-                networkError = jsonError
-            }
-
-            if error == nil {
-                self.isOffline = false
-                self.storeRequest(request!, data: jsonData!)
-                completionHandler(data: jsonData)
-                task.requestDidFinish()
-            } else {
-                task.requestDidFinishWithError(networkError!)
-            }
-        }
-
-        return task
+    func goOffline() {
+        self.isOffline = true
     }
-
-    private func storeRequest(urlRequest: NSURLRequest, data: AnyObject) {
-
-        if urlRequest.HTTPMethod != "GET" {
-            return
-        }
-
-        // save urlRequest.URL and data
+    
+    func goOnline() {
+        self.isOffline = false
     }
-
-    private func retrieveRequest(urlRequest: NSURLRequest) -> AnyObject? {
-        return nil
-        // return the data if they exist
-    }
-
  }
---
+++ GtsiapKit/Api/Requestable.swift
@@ -0,0 +1,94 @@
+//
+//  Requestable.swift
+//  GtsiapKit
+//
+//  Created by Giorgos Tsiapaliokas on 9/23/15.
+//  Copyright © 2015 Giorgos Tsiapaliokas. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+public protocol RequestableOfflineDelegate: class {
+    
+    func storeRequest(urlRequest: NSURLRequest, data: AnyObject)
+    func retrieveRequest(urlRequest: NSURLRequest) -> AnyObject?
+    
+}
+
+public protocol Requestable: ApiPresentable {
+        
+    weak var offlineDelegate: RequestableOfflineDelegate? { get set }
+    
+    func requestDidFinishWithError(error: NSError)
+    func requestDidFinishWithNoNetworkConnection()
+    func requestDidFinish()
+    
+}
+
+extension Requestable {
+    public func doRequest(
+        urlRequest: URLRequestConvertible,
+        completionHandler: (data: AnyObject?) -> Void
+    ) -> Request {
+            
+        return request(urlRequest).response {
+                (request, response, data, error) in
+                
+                // in future versions check for errors
+                networkLogger.debug("request: \(request)")
+                networkLogger.debug("response: \(response)")
+                networkLogger.debug("error: \(error)")
+                
+                var jsonError: NSError?
+                let jsonData: AnyObject?
+                do {
+                    jsonData = try NSJSONSerialization.JSONObjectWithData(data!,
+                        options: NSJSONReadingOptions.AllowFragments)
+                } catch let error as NSError {
+                    jsonError = error
+                    jsonData = nil
+                } catch {
+                    fatalError()
+                }
+                
+                networkLogger.debug("data: \(jsonData)")
+                
+                var networkError: NSError?
+                
+                // TODO
+                if let error = error as? NSError {
+                    
+                    if error.code == -1009 {
+                        // no internet connection is available
+                        ApiManager.sharedManager.goOffline()
+                        
+                        if let storedData: AnyObject = self.offlineDelegate?.retrieveRequest(request!) {
+                            completionHandler(data: storedData)
+                            self.requestDidFinish()
+                            
+                            return
+                        }
+                        
+                    self.requestDidFinishWithNoNetworkConnection()
+                        return
+                    }
+                    
+                    networkError = error
+                    
+                } else if jsonError != nil {
+                    networkError = jsonError
+                }
+                
+                if error == nil {
+                    ApiManager.sharedManager.goOnline()
+                    self.offlineDelegate?.storeRequest(request!, data: jsonData!)
+                    completionHandler(data: jsonData)
+                    self.requestDidFinish()
+                } else {
+                    self.requestDidFinishWithError(networkError!)
+                }
+            }
+    }
+    
+}
