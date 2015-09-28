6dd699b884cc61d008e8f0ec6eab10d460f6e97a
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-09-28 15:27:52 +0300

    fix build for swift 2.0

--- GtsiapKit/Operation/OperationQueue.swift
+++ GtsiapKit/Operation/OperationQueue.swift
@@ -15,8 +15,8 @@ public class OperationQueue: NSOperationQueue {
 
         addObserver(self, forKeyPath: "operationCount", options: NSKeyValueObservingOptions.New, context: nil)
     }
-
-    public override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+    
+    public override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
 
         if keyPath != "operationCount" {
             return
