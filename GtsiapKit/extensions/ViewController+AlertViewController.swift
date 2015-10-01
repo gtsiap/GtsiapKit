18a1e981fcf3341bf6b23778a5ed59030ade7731
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-10-01 15:34:07 +0300

    add extension show alert

---
+++ GtsiapKit/extensions/ViewController+AlertViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController+AlertViewController.swift
+//  GtsiapKit
+//
+//  Created by Giorgos Tsiapaliokas on 9/27/15.
+//  Copyright © 2015 Giorgos Tsiapaliokas. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+
+    public func showAlert(title: String, message: String) {
+        let alertVC = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .Alert
+        )
+        
+        let okAction = UIAlertAction(title: "Ok", style: .Default)
+        { alertAction in
+            alertVC.dismissViewControllerAnimated(true, completion: nil)
+        }
+    
+        alertVC.addAction(okAction)
+        presentViewController(alertVC, animated: true, completion: nil)
+    }
+    
+}
