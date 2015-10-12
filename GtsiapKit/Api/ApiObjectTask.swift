155d27afd744ad130c04577372439086033a8d1c
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-10-12 19:06:33 +0300

    add ApiObjectTask

---
+++ GtsiapKit/Api/ApiObjectTask.swift
@@ -0,0 +1,36 @@
+//
+//  ApiObjectTask.swift
+//  GtsiapKit
+//
+//  Created by Giorgos Tsiapaliokas on 12/10/15.
+//  Copyright © 2015 Giorgos Tsiapaliokas. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+public class ApiObjectTask<T>: ApiTask {
+    public private(set) var taskResultProvider: ApiTaskResultProvider<T>!
+    
+    override init() {
+        super.init()
+        self.taskResultProvider = ApiTaskResultProvider<T>(task: self)
+    }
+    
+    public func retrieveObject(
+        completionHandler: (result: ApiTaskResultProvider<T>) -> ())
+        -> ApiObjectTask<T>
+    {
+        if self.taskResultProvider.hasDataAvailable {
+            completionHandler(result: self.taskResultProvider)
+            return self
+        }
+        
+        self.request = doRequest(self.urlRequest!) { data in
+            self.taskResultProvider.data = data
+            completionHandler(result: self.taskResultProvider)
+        }
+        
+        return self
+    }
+}
