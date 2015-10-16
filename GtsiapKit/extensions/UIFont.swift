c22a4561ee23e2fb850216ce41687c7f60f75837
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-10-16 12:52:58 +0300

    add UIFont extensions

---
+++ GtsiapKit/extensions/UIFont.swift
@@ -0,0 +1,25 @@
+//
+//  UIFont.swift
+//  GtsiapKit
+//
+//  Created by Giorgos Tsiapaliokas on 16/10/15.
+//  Copyright © 2015 Giorgos Tsiapaliokas. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+
+    public class func normalBoldFont() -> UIFont {
+        return ThemeManager.defaultTheme.normalBoldFont()
+    }
+
+    public class func smallBoldFont() -> UIFont {
+        return ThemeManager.defaultTheme.smallBoldFont()
+    }
+
+    public class func headlineFont() -> UIFont {
+        return ThemeManager.defaultTheme.headlineFont()
+    }
+
+}
