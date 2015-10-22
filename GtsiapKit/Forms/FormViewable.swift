f515da7220a677919803d756164b32f9b28e01ed
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-10-22 22:24:48 +0300

    add FormStackView

--- GtsiapKit/Forms/FormViewable.swift
+++ GtsiapKit/Forms/FormViewable.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2015 Giorgos Tsiapaliokas. All rights reserved.
 //
 
+import SnapKit
+
 public protocol FormViewable {
     var resultChanged: ((AnyObject?) -> ())? { get set}
     var required: Bool { get set }
@@ -14,4 +16,38 @@ public protocol FormViewable {
 public class FormView: UIView, FormViewable {
     public var resultChanged: ((AnyObject?) -> ())?
     public var required: Bool = true
+}
+
+public class FormStackView: FormView {
+
+    public var formViews: [FormView] = [FormView]() {
+        didSet {
+            setupViews()
+        }
+    }
+
+    private func setupViews() {
+        self.subviews.forEach() { $0.removeFromSuperview() }
+
+        var previousView: UIView?
+        for view in self.formViews {
+            addSubview(view)
+
+            view.snp_makeConstraints() { make in
+                if let pv = previousView {
+                    make.top.equalTo(pv.snp_bottom)
+                } else {
+                    make.top.equalTo(self)
+                } // end if
+            } // end snp_makeConstraints
+
+            previousView = view
+        } // end for
+
+        previousView?.snp_remakeConstraints() { make in
+            make.bottom.equalTo(self)
+        }
+
+    } // end func
+
 }
