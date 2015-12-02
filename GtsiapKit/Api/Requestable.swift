46c482610c6da1593f86d06c740d785e9423d26b
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-12-02 12:24:09 +0200

    add tests

--- GtsiapKit/Api/Requestable.swift
+++ GtsiapKit/Api/Requestable.swift
@@ -21,8 +21,32 @@
 import UIKit
 import Alamofire
 
-public protocol RequestableOfflineDelegate: class {
+private struct RequestableDebugKey {
+    static var debugKey = "requestable_debug_key"
+}
 
+// we want to use objc_getAssociatedObject/objc_setAssociatedObject
+// so this one must be a class
+enum RequestableDebugType {
+    case JSONDeserializationError
+    case NetworkError
+    case NoInternetConnection
+}
+
+class RequestableDebug {
+    let debugType: RequestableDebugType
+    let debugHandler: () -> ()
+    init(
+        _ requestableDebugType: RequestableDebugType,
+        debugHandler: () -> ()
+    ) {
+        self.debugType = requestableDebugType
+        self.debugHandler = debugHandler
+    }
+}
+
+public protocol RequestableOfflineDelegate: class {
+    
     func storeRequest(urlRequest: NSURLRequest, data: [String : AnyObject])
     func retrieveRequest(urlRequest: NSURLRequest) -> [String : AnyObject]?
 
@@ -31,7 +55,7 @@ public protocol RequestableOfflineDelegate: class {
 public protocol Requestable: ApiPresentable {
 
     weak var offlineDelegate: RequestableOfflineDelegate? { get set }
-
+    
     func requestDidFinishWithError(error: NSError)
     func requestDidFinishWithNoNetworkConnection()
     func requestDidFinish()
@@ -41,67 +65,107 @@ extension Requestable {
 
     func doRequest(
         urlRequest: URLRequestConvertible,
-        completionHandler: (data: [String : AnyObject]) -> Void
+        completionHandler: (data: [String : AnyObject]) -> ()
     ) -> Request {
 
-        return request(urlRequest).response {
-                (request, response, data, error) in
+        return request(urlRequest).response
+        { (request, response, data, error) in
 
-                // in future versions check for errors
-                networkLogger.debug("request: \(request)")
-                networkLogger.debug("response: \(response)")
-                networkLogger.debug("error: \(error)")
-
-                var jsonError: NSError?
-                let jsonData: [String : AnyObject]?
-                do {
-                    jsonData = try NSJSONSerialization.JSONObjectWithData(data!,
-                        options: NSJSONReadingOptions.AllowFragments)
-                    as? [String : AnyObject]
-                } catch let error as NSError {
-                    jsonError = error
-                    jsonData = nil
-                } catch {
-                    fatalError()
-                }
-
-                networkLogger.debug("data: \(jsonData)")
-
-                var networkError: NSError?
-
-                // TODO
-                if let error = error {
-
-                    if error.code == -1009 {
-                        // no internet connection is available
-                        ApiManager.sharedManager.goOffline()
-
-                        if let storedData: [String : AnyObject] = self.offlineDelegate?.retrieveRequest(request!) {
-                            completionHandler(data: storedData)
-                            self.requestDidFinish()
-
-                            return
-                        }
-
-                        self.requestDidFinishWithNoNetworkConnection()
-                        return
-                    }
-
-                    networkError = error
-
-                } else if jsonError != nil {
-                    networkError = jsonError
-                }
-
-                if error == nil {
-                    ApiManager.sharedManager.goOnline()
-                    self.offlineDelegate?.storeRequest(request!, data: jsonData!)
-                    completionHandler(data: jsonData!)
-                    self.requestDidFinish()
-                } else {
-                    self.requestDidFinishWithError(networkError!)
-                }
+            func handleError(error: NSError) {
+                self.requestDidFinishWithError(error)
             }
+            
+            func handleNoNetworkConnection() {
+                // no internet connection
+                    
+                ApiManager.sharedManager.goOffline()
+                    
+                guard let
+                    offlineDelegate = self.offlineDelegate,
+                    request = request,
+                    offlineData = offlineDelegate.retrieveRequest(request)
+                else {
+                    self.requestDidFinishWithNoNetworkConnection()
+                    return
+                }
+                    
+                completionHandler(data: offlineData)
+                self.requestDidFinish()
+            }
+            
+            networkLogger.debug("request: \(request)")
+            networkLogger.debug("response: \(response)")
+            networkLogger.debug("error: \(error)")
+            
+            if let debug = self.debug {
+                switch debug.debugType {
+                case .NoInternetConnection:
+                    handleNoNetworkConnection()
+                    debug.debugHandler()
+                case .NetworkError:
+                    handleError(NSError(
+                        domain: "requestable.debug.networkError",
+                        code: 1111,
+                        userInfo: nil)
+                    )
+                    debug.debugHandler()
+                case .JSONDeserializationError:
+                    handleError(NSError(
+                        domain: "requestable.debug.jsonError",
+                        code: 2222,
+                        userInfo: nil)
+                    )
+                    debug.debugHandler()
+                }
+                
+                return
+            }
+            
+            if let error = error where error.code == -1009 {
+                handleNoNetworkConnection()
+                return
+            } else if let error = error {
+                handleError(error)
+                return
+            }
+            
+            let jsonData: [String : AnyObject]?
+            do {
+                jsonData = try NSJSONSerialization.JSONObjectWithData(
+                    data!,
+                    options: NSJSONReadingOptions.AllowFragments
+                ) as? [String : AnyObject]
+                
+                ApiManager.sharedManager.goOnline()
+                networkLogger.debug("data: \(jsonData)")
+                self.offlineDelegate?.storeRequest(request!, data: jsonData!)
+
+                completionHandler(data: jsonData!)
+                self.requestDidFinish()
+            } catch let error as NSError {
+                self.requestDidFinishWithError(error)
+                return
+            }
+        }
+    }
+    
+    var debug: RequestableDebug? {
+        get {
+            return objc_getAssociatedObject(
+                self,
+                &RequestableDebugKey.debugKey
+            )as? RequestableDebug
+        }
+        
+        set(newValue) {
+            objc_setAssociatedObject(
+                self,
+                &RequestableDebugKey.debugKey,
+                newValue,
+                .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+            )
+        }
+        
     }
 
 }
