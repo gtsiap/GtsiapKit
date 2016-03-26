d8dd29fd1bb300bc81646c5765f28bf292808300
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2016-03-26 16:34:12 +0200

    copyright

--- GtsiapKit/Theme/Theme.swift
+++ GtsiapKit/Theme/Theme.swift
@@ -1,5 +1,4 @@
-
-// Copyright (c) 2015 Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
+// Copyright (c) 2015-2016 Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,13 +17,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
-//
-//  Theme.swift
-//
-//  Created by Giorgos Tsiapaliokas on 5/25/15.
-//  Copyright (c) 2015 Giorgos Tsiapaliokas. All rights reserved.
-//
 
 import UIKit
 
